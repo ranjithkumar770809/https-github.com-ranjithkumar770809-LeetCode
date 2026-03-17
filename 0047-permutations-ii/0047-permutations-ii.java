2d41ca349b27fc80cab1aa81ebe0de3ea6dbaf79
ranjithkumar770809 <ranjithkumar770809@gmail.com>
2026-03-17 13:21:40 +0530

    Time: 704 ms (5.01%), Space: 46.7 MB (57.94%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -1,36 +1,35 @@
 class Solution {
-    public List<List<Integer>> permuteUnique(int[] nums) {
-        Set<List<Integer>> s = new HashSet<>();
-        helper(nums,0,s);
-        return new ArrayList<>(s);
-    }
-    static void helper(int[] nums,int idx,Set<List<Integer>> s){
-        if(idx==nums.length){
-           List<Integer> list = new ArrayList<>();
-            for (int n : nums) list.add(n);
-            s.add(list);
-            return;
-        }
-        for(int i=idx;i<nums.length;i++){
-            swap(nums,idx,i);
-            helper(nums,idx+1,s);
-            swap(nums,idx,i);
-        }
 
-    }
-    static void swap(int[] nums,int i,int j){
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
-     static {
-        Runtime.getRuntime().gc();
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            try (FileWriter f = new FileWriter("display_runtime.txt")) {
-                f.write("0");
-            } catch (Exception e) {
-
+    public void backtrack(List<List<Integer>> result, List<Integer> temp, int[] nums,boolean[] visited)
+    {
+        if(temp.size()==nums.length)
+        {
+            if(!result.contains(temp))
+            {
+                result.add(new ArrayList<>(temp));
+                return;
             }
-        }));
+        }
+
+        for (int i=0 ;i<nums.length;i++)
+        {
+            if(!visited[i])
+            {
+                temp.add(nums[i]);
+                visited[i] = true;
+                backtrack(result,temp,nums,visited);
+                temp.remove(temp.size()-1); // Backtrack
+                visited[i] = false;
+            }
+        }
     }
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+        boolean[] visited = new boolean[nums.length];
+        backtrack(result,temp,nums,visited);
+        return result;
+    }
+
 }
