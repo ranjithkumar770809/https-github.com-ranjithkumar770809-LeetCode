901378e444a689d4cf64fc672f0d8e70915ca425
ranjithkumar770809 <ranjithkumar770809@gmail.com>
2026-03-25 12:23:58 +0530

    Time: 7 ms (35.31%), Space: 46.3 MB (99.04%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,26 +1,32 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
+        
         // Step:1 { ArrayList }
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for(int i=0;i<numCourses;i++){  
+        for(int i=0;i<numCourses;i++)
+        {  
             adj.add(new ArrayList<>());
         }
 
-        for(int[] p: prerequisites){
+        for(int[] p: prerequisites)
+        {
             adj.get(p[1]).add(p[0]);
         }
 
         // Step:2 { Indegree }
         int[] ind = new int[numCourses];        
-        for(int i=0;i<numCourses;i++){
-            for (int n : adj.get(i)){
+        for(int i=0;i<numCourses;i++)
+        {
+            for (int n : adj.get(i))
+            {
                 ind[n]++;
             }
         }
 
         // Step:3 { Queue }
         Queue<Integer> q = new LinkedList<>(); 
-        for (int i = 0; i <numCourses; i++) {
+        for (int i = 0; i <numCourses; i++) 
+        {
             if (ind[i] == 0) {
                 q.add(i);
             }
@@ -29,17 +35,19 @@ class Solution {
         // Step:4 { BFS }
         int[] op = new int[numCourses];
         int index = 0;
+        
         while(!q.isEmpty()){                    
             int vertex = q.poll();
             op[index++] = vertex;
-
-            for(int nei : adj.get(vertex)){
+            for(int nei : adj.get(vertex))
+            {
                 ind[nei]--;
-                if(ind[nei]==0){
-                    q.add(nei);
+                if(ind[nei]==0){ 
+                    q.add(nei); 
                 }
             }
         }
+        
         if (index == numCourses) return op;
         return new int[]{};
     }
