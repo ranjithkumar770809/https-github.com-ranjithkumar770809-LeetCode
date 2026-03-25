208f8e7490c7f13f3dd3530a7ca40ab4f322177f
ranjithkumar770809 <ranjithkumar770809@gmail.com>
2026-03-25 12:02:35 +0530

    Time: 106 ms (5.17%), Space: 46.8 MB (64.43%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for(int i=0;i<numCourses;i++){           // Step:1 { ArrayList }
+            adj.add(new ArrayList<>());
+        }
+
+        for(int[] edge: prerequisites){
+            adj.get(edge[1]).add(edge[0]);
+        }
+
+        int[] ind = new int[numCourses];         // Step:2 { Indegree }
+        for(int i=0;i<numCourses;i++){
+            for (int n : adj.get(i)){
+                ind[n]++;
+            }
+        }
+
+        Queue<Integer> q = new LinkedList<>();  // Step:3 { Queue }
+        for (int i = 0; i <numCourses; i++) {
+            if (ind[i] == 0) {
+                q.add(i);
+            }
+        }
+
+        ArrayList<Integer> res = new  ArrayList<>();
+        int[] op = new int[numCourses];
+        int index = 0;
+        while(!q.isEmpty()){                    // Step:4 { BFS }
+            int vertex = q.poll();
+            op[index++] = vertex;
+            for(int nei : adj.get(vertex)){
+                ind[nei]--;
+                if(ind[nei]==0){
+                    q.add(nei);
+                }
+            }
+        }
+        if (index == numCourses) return op;
+        return new int[]{};
+    }
+}
