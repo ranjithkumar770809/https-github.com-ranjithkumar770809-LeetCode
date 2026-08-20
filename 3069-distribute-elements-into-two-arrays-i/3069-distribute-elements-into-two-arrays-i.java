3c89a525023f2892d3fa9c4d576e9cb28a5b6cdf
ranjithkumar770809 <ranjithkumar770809@gmail.com>
2026-08-20 15:13:52 +0530

    Time: 2 ms (57.45%), Space: 47 MB (12.74%) - LeetHub

--- 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.java
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.java
@@ -4,13 +4,8 @@ class Solution {
     public int[] resultArray(int[] nums) {
         List<Integer> arr1 = new ArrayList<>();
         List<Integer> arr2 = new ArrayList<>();
-
-        // First operation: append nums[0] to arr1
         arr1.add(nums[0]);
-        // Second operation: append nums[1] to arr2
         arr2.add(nums[1]);
-
-        // Sequential comparisons for the rest of the elements
         for (int i = 2; i < nums.length; i++) {
             if (arr1.get(arr1.size() - 1) > arr2.get(arr2.size() - 1)) {
                 arr1.add(nums[i]);
@@ -18,8 +13,6 @@ class Solution {
                 arr2.add(nums[i]);
             }
         }
-
-        // Merge arr1 followed by arr2 into result array
         int[] result = new int[nums.length];
         int index = 0;
 
