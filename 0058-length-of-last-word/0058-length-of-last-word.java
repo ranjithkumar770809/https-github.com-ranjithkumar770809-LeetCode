da66041caafd739dd4c3cda99182e960bf1a1ac8
ranjithkumar770809 <ranjithkumar770809@gmail.com>
2026-08-21 09:46:34 +0530

    Time: 0 ms (100%), Space: 42.4 MB (99.53%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        s=s.trim();
+        int n=s.length();
+        for(int i=n-1;i>=0;i--){
+            if(s.charAt(i)==' '){
+                return n-1-i;
+            }
+        }
+
+        return n;
+    }
+}
