1b93df29d4b44aaf854c749583221b926ff775ad
ranjithkumar770809 <ranjithkumar770809@gmail.com>
2026-08-25 12:04:39 +0530

    Time: 4 ms (43.68%), Space: 44.6 MB (70.01%) - LeetHub

---
+++ 1002-find-common-characters/1002-find-common-characters.java
@@ -0,0 +1,24 @@
+class Solution {
+public List<String> commonChars(String[] A) {
+        List<String> ans = new ArrayList<>();
+        int[] dict = new int[26];
+        for (int j = 0; j < A[0].length(); j++) {
+            dict[A[0].charAt(j) - 'a']++;
+        }
+        for (int i = 1; i < A.length; i++) {
+            int[] curr = new int[26];
+            for (int j = 0; j < A[i].length(); j++) {
+                curr[A[i].charAt(j) - 'a']++;
+            }
+            for (int j = 0; j < 26; j++) {
+                if (curr[j] < dict[j]) dict[j] = curr[j];
+            }
+        }
+        for (int i = 0; i < 26; i++) {
+            for (int j = 0; j < dict[i]; j++) {
+                ans.add(Character.toString((char) ('a' + i)));
+            }
+        }
+        return ans;
+    }
+}
