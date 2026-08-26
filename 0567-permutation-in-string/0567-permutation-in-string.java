ffe78496a711c93a75afd9626b10a3bc87697741
ranjithkumar770809 <ranjithkumar770809@gmail.com>
2026-08-26 19:38:14 +0530

    Time: 5 ms (97.47%), Space: 43.9 MB (73.79%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -8,22 +8,18 @@ class Solution {
         int[] s1Counts = new int[26];
         int[] s2Counts = new int[26];
 
-        // Fill initial frequency tables for s1 and the first window in s2
         for (int i = 0; i < n1; i++) {
             s1Counts[s1.charAt(i) - 'a']++;
             s2Counts[s2.charAt(i) - 'a']++;
         }
-
-        // Slide the window across s2
+ 
         for (int i = 0; i < n2 - n1; i++) {
             if (matches(s1Counts, s2Counts)) return true;
 
-            // Slide window: add character at (i + n1) and remove character at i
-            s2Counts[s2.charAt(i + n1) - 'a']++;
+             s2Counts[s2.charAt(i + n1) - 'a']++;
             s2Counts[s2.charAt(i) - 'a']--;
         }
-
-        // Check the last window
+ 
         return matches(s1Counts, s2Counts);
     }
 
